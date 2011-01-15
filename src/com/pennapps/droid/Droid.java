cd764de167c13ab2fdff007ea4045f398a215f25
Thanat Owlarn <thanat.owlarn@gmail.com>
2011-01-15 16:23:28 -0500

    ffdh orientation stuffs

--- src/com/pennapps/droid/Droid.java
+++ src/com/pennapps/droid/Droid.java
@@ -11,22 +11,22 @@ import android.widget.Toast;
 
 public class Droid extends Activity implements SensorEventListener {
 	// Accelerometer X, Y, and Z values
-	private TextView accelXValue;
-	private TextView accelYValue;
-	private TextView accelZValue;
-	private TextView linaccelXValue;
-	private TextView linaccelYValue;
-	private TextView linaccelZValue;
+	private TextView xorient_tv;
+	private TextView yorient_tv;
+	private TextView zorient_tv;
+	private TextView xorientv_tv;
+	private TextView yorientv_tv;
+	private TextView zorientv_tv;
 	
-	private float x, y, z;
-	private float last_x, last_y, last_z;
+	private float x=0, y=0, z=0;
+	private float last_x=0, last_y=0, last_z=0;
 	private long xtime, ytime, ztime;
     private long lastUpdate = -1;
     
     /* Detection constants -- change to tweak performance */
 	private static final long TIME_THRESHOLD = 1000;
-	private static final float MOVEMENT_THRESHOLD = 6;
-    private static final float FILTERING_FACTOR = 0.3f;
+	private static final float VELOCITY_THRESHOLD = 0.5f;
+    private static final float FILTERING_FACTOR = 0.8f;
 
 	// Orientation X, Y, and Z values
 	private TextView orientXValue;
@@ -44,12 +44,12 @@ public class Droid extends Activity implements SensorEventListener {
 		setContentView(R.layout.main);
 
 		// Capture accelerometer related view elements
-		accelXValue = (TextView) findViewById(R.id.accel_x_value);
-		accelYValue = (TextView) findViewById(R.id.accel_y_value);
-		accelZValue = (TextView) findViewById(R.id.accel_z_value);
-		linaccelXValue = (TextView) findViewById(R.id.linaccel_x_value);
-		linaccelYValue = (TextView) findViewById(R.id.linaccel_y_value);
-		linaccelZValue = (TextView) findViewById(R.id.linaccel_z_value);
+		xorient_tv = (TextView) findViewById(R.id.accel_x_value);
+		yorient_tv = (TextView) findViewById(R.id.accel_y_value);
+		zorient_tv = (TextView) findViewById(R.id.accel_z_value);
+		xorientv_tv = (TextView) findViewById(R.id.linaccel_x_value);
+		yorientv_tv = (TextView) findViewById(R.id.linaccel_y_value);
+		zorientv_tv = (TextView) findViewById(R.id.linaccel_z_value);
 
 		// Capture orientation related view elements
 		orientXValue = (TextView) findViewById(R.id.orient_x_value);
@@ -57,12 +57,12 @@ public class Droid extends Activity implements SensorEventListener {
 		orientZValue = (TextView) findViewById(R.id.orient_z_value);
 
 		// Initialize accelerometer related view elements
-		accelXValue.setText("0.00");
-		accelYValue.setText("0.00");
-		accelZValue.setText("0.00");
-		linaccelXValue.setText("0.00");
-		linaccelYValue.setText("0.00");
-		linaccelZValue.setText("0.00");
+		xorient_tv.setText("0.00");
+		yorient_tv.setText("0.00");
+		zorient_tv.setText("0.00");
+		xorientv_tv.setText("0.00");
+		yorientv_tv.setText("0.00");
+		zorientv_tv.setText("0.00");
 
 		// Initialize orientation related view elements
 		orientXValue.setText("0.00");
@@ -73,71 +73,57 @@ public class Droid extends Activity implements SensorEventListener {
 	// This method will update the UI on new sensor events
 	public void onSensorChanged(SensorEvent sensorEvent) {
 		synchronized (this) {
-					
-			if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-				accelXValue.setText(Float.toString(sensorEvent.values[0]));
-				accelYValue.setText(Float.toString(sensorEvent.values[1]));
-				accelZValue.setText(Float.toString(sensorEvent.values[2]));
+			long curTime = System.currentTimeMillis();		
+			if (sensorEvent.sensor.getType() == Sensor.TYPE_ORIENTATION) {
+				xorient_tv.setText(Float.toString(sensorEvent.values[0]));
+				yorient_tv.setText(Float.toString(sensorEvent.values[1]));
+				zorient_tv.setText(Float.toString(sensorEvent.values[2]));
 				
-				long curTime = System.currentTimeMillis();
 				// only allow one update every 100ms.
 				if ((curTime - lastUpdate) > 100) {
 					long diffTime = (curTime - lastUpdate);
 					lastUpdate = curTime;
 					
-					float[] values = sensorEvent.values;
-					
+					// CURRENT VALUES
 					x = sensorEvent.values[0];
 					y = sensorEvent.values[1];
 					z = sensorEvent.values[2];
 					
+					// EXPONENTIAL AVERAGING 
 					last_x = (x * FILTERING_FACTOR) + (last_x * (1.0f - FILTERING_FACTOR));
 					last_y = (y * FILTERING_FACTOR) + (last_y * (1.0f - FILTERING_FACTOR));
 					last_z = (z * FILTERING_FACTOR) + (last_z * (1.0f - FILTERING_FACTOR));
 
-					float xspeed = x - last_x;
-					float yspeed = y - last_y;
-					float zspeed = z - last_z;
-					if (xspeed > MOVEMENT_THRESHOLD && (curTime-xtime > TIME_THRESHOLD) ) {
-						xtime = curTime;
-						Toast.makeText(getApplicationContext(), "X direction1", Toast.LENGTH_SHORT).show();
-					}
-					else if (xspeed < -MOVEMENT_THRESHOLD && (curTime-xtime > TIME_THRESHOLD)) {
-						xtime = curTime;
-						Toast.makeText(getApplicationContext(), "X direction2", Toast.LENGTH_SHORT).show();
-					}
-					if (yspeed > MOVEMENT_THRESHOLD && (curTime-ytime > TIME_THRESHOLD)) {
-						ytime = curTime;
-						Toast.makeText(getApplicationContext(), "Y direction1", Toast.LENGTH_SHORT).show();
-					}
-					else if (yspeed < -MOVEMENT_THRESHOLD && (curTime-ytime > TIME_THRESHOLD)) {
-						ytime = curTime;
-						Toast.makeText(getApplicationContext(), "Y direction2", Toast.LENGTH_SHORT).show();
-					}
-					if (zspeed > MOVEMENT_THRESHOLD && (curTime-ztime > TIME_THRESHOLD)) {
-						ztime = curTime;
-						Toast.makeText(getApplicationContext(), "Z direction1", Toast.LENGTH_SHORT).show();
-					}
-					else if (zspeed < -MOVEMENT_THRESHOLD && (curTime-ztime > TIME_THRESHOLD)) {
-						ztime = curTime;
-						Toast.makeText(getApplicationContext(), "Z direction2", Toast.LENGTH_SHORT).show();
-					}
+					float xspeed = (x - last_x);
+					float yspeed = (y - last_y);
+					float zspeed = (z - last_z);
 					
-					linaccelXValue.setText(Float.toString(xspeed));
-					linaccelYValue.setText(Float.toString(yspeed));
-					linaccelZValue.setText(Float.toString(zspeed));
+					// current position
+					xorient_tv.setText(Float.toString(x));
+					yorient_tv.setText(Float.toString(y));
+					zorient_tv.setText(Float.toString(z));
+					
+					// incremental velocity
+					xorientv_tv.setText(Float.toString(xspeed));
+					yorientv_tv.setText(Float.toString(yspeed));
+					zorientv_tv.setText(Float.toString(zspeed));
+					
+					// averaged positions
+					if (xspeed > VELOCITY_THRESHOLD || xspeed < -VELOCITY_THRESHOLD){
+						orientXValue.setText(Float.toString(last_x));
+					}
+					if (yspeed > VELOCITY_THRESHOLD || yspeed < -VELOCITY_THRESHOLD){
+						orientYValue.setText(Float.toString(last_y));
+					}
+					if (zspeed > VELOCITY_THRESHOLD || zspeed < -VELOCITY_THRESHOLD){
+						orientZValue.setText(Float.toString(last_z));
+					}
 					
 					/*
 					last_x = x;
 					last_y = y;
 					last_z = z;*/
 				}
-			}
-
-			if (sensorEvent.sensor.getType() == Sensor.TYPE_ORIENTATION) {
-				orientXValue.setText(Float.toString(sensorEvent.values[0]));
-				orientYValue.setText(Float.toString(sensorEvent.values[1]));
-				orientZValue.setText(Float.toString(sensorEvent.values[2]));       
 			}
 		}
 	}
@@ -150,8 +136,8 @@ public class Droid extends Activity implements SensorEventListener {
 	protected void onResume() {
 		super.onResume();
 		// Register this class as a listener for the accelerometer sensor
-		sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
-		sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_NORMAL);
+		sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_UI);
+		sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_UI);
 	}
 
 	@Override
@@ -159,6 +145,13 @@ public class Droid extends Activity implements SensorEventListener {
 		// Unregister the listener
 		sensorManager.unregisterListener(this);
 		super.onStop();
+	}
+	
+	@Override
+	protected void onPause() {
+		// Unregister the listener
+		sensorManager.unregisterListener(this);
+		super.onPause();		
 	}
 
 }
