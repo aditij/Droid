bf8b645cf404f10d831c4eea7c2d6aa05b24c7ec
Aditi Jain <aditij89@gmail.com>
2011-01-14 21:32:30 -0500

    initial commit

---
+++ src/com/pennapps/droid/Droid.java
@@ -0,0 +1,13 @@
+package com.pennapps.droid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Droid extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
